bf956bf4a62182bb933c3c43b780323eb4368272
TraffiXTon <leo.muscle28@gmail.com>
2024-08-21 17:03:12 +0100

    Minor fixes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,5 +3,5 @@ export const SERVICE_ID = (import.meta.env.VITE_SERVICE_ID);
 export const PUBLIC_KEY = (import.meta.env.VITE_PUBLIC_KEY);
 
 export const GITHUB_LINK = (import.meta.env.VITE_GITHUB_LINK);
-export const TELEGRAM_LINK = (import.meta.env.VITE_TELEGRAM_LINK);
 export const LINKEDIN_LINK = (import.meta.env.VITE_LINKEDIN_LINK);
+export const TELEGRAM_LINK = (import.meta.env.VITE_TELEGRAM_LINK);
