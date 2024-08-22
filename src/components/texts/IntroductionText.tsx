3b77b4c8e1188867314fe38dc5e7e4a64c39030e
TraffiXTon <leo.muscle28@gmail.com>
2024-08-22 18:12:46 +0100

    Fixes

--- src/components/texts/IntroductionText.tsx
+++ src/components/texts/IntroductionText.tsx
@@ -8,7 +8,7 @@ const IntroductionText = () => {
         <div className={"font-interFont select-none mx-5"}>
             <TextAnimation classname={"text-4xl md:text-5xl lg:text-7xl font-extrabold"} text={hello}/>
             <TextAnimation classname={"text-6xl md:text-6xl lg:text-8xl my-1 font-extrabold"} text={hello2}/>
-            <TextAnimation classname={"text-2xl md:text-3xl lg:text-5xl ml-7 mob1:ml-16 mob3:ml-36 md:ml-40 lg:ml-52 mt-5 font-bold"} text={hello3}/>
+            <TextAnimation classname={"text-2xl md:text-3xl lg:text-5xl ml-5 mob1:ml-16 mob3:ml-36 md:ml-40 lg:ml-52 mt-5 font-bold"} text={hello3}/>
         </div>
     );
 };
