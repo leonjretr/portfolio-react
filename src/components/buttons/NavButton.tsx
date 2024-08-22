8b2c35ebea27770b2f8e1e89e178fd90e710a414
TraffiXTon <leo.muscle28@gmail.com>
2024-08-22 18:05:59 +0100

    Fixes

--- src/components/buttons/NavButton.tsx
+++ src/components/buttons/NavButton.tsx
@@ -1,18 +1,18 @@
 import {motion} from "framer-motion";
 import {FC} from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 
 interface NavButtonProps {
     text: string;
-    linkTo:string;
+    // linkTo:string;
 }
 
-const NavButton: FC<NavButtonProps> = ({text, linkTo}) => {
+const NavButton: FC<NavButtonProps> = ({text}) => {
     return (
         <motion.button className={"hover:bg-gray-100 hover:rounded-lg p-1"}
                        whileHover={{scale: 1.2}}
                        whileTap={{scale: 0.9}}
-                       style={{filter: "blur(.0px)"}}>
+                       style={{filter: "blur(.0px)"}}>{text}
             {/*<Link to={linkTo}>{text}</Link>*/}
         </motion.button>
     );
