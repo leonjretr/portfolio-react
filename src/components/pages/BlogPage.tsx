5e287d73b1ee1ab53837cdbf72bac7c8d1a7324d
Leon <leo.muscle28@gmail.com>
2025-11-06 17:43:20 +0000

    Added: Improved animation on photos in MosaicGallery.tsx, added description zone

--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -6,7 +6,7 @@ const BlogPage = () => {
         <div className={"min-h-screen bg-white dark:bg-bgDarkColor scroll-smooth"}>
             <PageWrapper>
                 <div className={"flex flex-col items-center justify-center text-3xl font-bold font-interFont dark:text-white m-5"}>
-                    Im
+                    Every image has its story✨
                     {/*<p> they may not be great, but I feel that they reflect my thought, my vision and myself. </p>*/}
                 </div>
                 <MosaicGallery/>
