2b3718de93032ad34147ef2f204fb7ad3f5f344e
Leon <leo.muscle28@gmail.com>
2026-09-11 15:48:38 +0100

    changed: a couple of new photos added;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router} from "react-router-dom";
 import MyRouting from "./components/routing/MyRouting.tsx";
-import {Analytics} from "@vercel/analytics/next"
+import {Analytics} from "@vercel/analytics/react"
 
 function App() {
     return (
